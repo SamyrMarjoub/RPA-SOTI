248084f194f05e32af87ef2d6696ce1f0991d16b
Samyr Marjoub <96318513+SamyrMarjoub@users.noreply.github.com>
2023-04-05 17:03:47 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 import puppeteer from "puppeteer";
+import ftp from 'basic-ftp'
+// const cron = require('node-cron');
+import cron from 'node-cron'
+// const fs = require('fs');
+// const path = require('path');
+import fs from 'fs'
+import path from 'path'
 
 function start() {
     (async () => {
@@ -6,10 +13,10 @@ function start() {
         const page = await browser.newPage();
         const dataAtual = new Date();
         const dataAtual2 = new Date();
-       
+
         dataAtual2.setDate(1);
         const diaF = dataAtual2.getDate().toString().padStart(2, '0');
-        const mesF = (dataAtual2.getMonth() + 1).toString().padStart(2, '0'); 
+        const mesF = (dataAtual2.getMonth() + 1).toString().padStart(2, '0');
         const anoF = dataAtual2.getFullYear();
         const primeiroDiaDoMes = `${diaF}/${mesF}/${anoF}`;
         const dia = dataAtual.getDate().toString().padStart(2, '0');
@@ -50,8 +57,27 @@ function start() {
                     // await page.waitForSelector(ref)
                     await page.click(ref)
                     console.log("Baixado")
-                    setTimeout(async () => {
+                    const client = new ftp.Client();
+                    client.ftp.verbose = true;
+                    try {
+                        await client.access({
+                            host: "57400550edb3.sn.mynetname.net/",
+                            user: "soti",
+                            password: "MZO&xU543wt#",
+                            secure: true
+                        });
+                        const downloadDir = path.join(process.env.HOME, 'Downloads');
+                        const files = fs.readdirSync(downloadDir);
+                        const firstFile = files.find(file => fs.statSync(path.join(downloadDir, file)).isFile());
 
+                        // fazer upload do arquivo encontrado
+                        await client.uploadFrom(path.join(downloadDir, firstFile), `/${firstFile}`);
+
+                    } catch (err) {
+                        console.log(err);
+                    }
+                    client.close();
+                    setTimeout(async () => {
                         await browser.close()
                     }, 10000);
 
@@ -66,6 +92,10 @@ function start() {
 
 start()
 
-setInterval(() => {
+cron.schedule("0 7,11,17 * * *", ()=>{
     start()
-}, 86400000 - 1)
+})
+
+// setInterval(() => {
+//     start()
+// }, 86400000 - 1)
